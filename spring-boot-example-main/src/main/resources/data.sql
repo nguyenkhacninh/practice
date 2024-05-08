91c9fb8224ac3fb02ff6196bc9b9c34ff1517eee
pc <nguyenkhacninhftu@gmail.com>
2024-05-08 16:02:31 +0700

    commit

---
+++ spring-boot-example-main/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into currencies (currency, description, exchange_rate) values('USD', 'United States Dollar', 167);
+insert into currencies (currency, description, exchange_rate) values('GBP', 'British Pound Sterling', 167);
+insert into currencies (currency, description, exchange_rate) values('EUR', 'Euro', 167);
